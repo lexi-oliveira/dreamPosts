b0a082c335d1a5554fd708d2660846073a0b5261
Mateus Oliveira <mateus.oliveira@aluno.ufop.ed.br>
2021-05-10 00:36:57 -0300

    Fetching a post list

--- lib/main.dart
+++ lib/main.dart
@@ -22,23 +22,33 @@ class DreamPosts extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-  /* Future<http.Response> fetchPost() {
-    return http.get(Uri.https('jsonplaceholder.typicode.com', 'post/1'));
-  } */
+  var posts = new List<Post>();
+
+  HomePage() {
+    posts = [];
+  }
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<Post> fetchPost() async {
+  Future fetchPost() async {
     final response =
-        await http.get(Uri.https('jsonplaceholder.typicode.com', 'posts/1'));
+        await http.get(Uri.https('jsonplaceholder.typicode.com', 'posts/'));
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
       // then parse the JSON.
-      return Post.fromJson(jsonDecode(response.body));
+      /* List<UserModel> users = (json.decode(response.body) as List).map((data) => UserModel.fromJson(data)).toList();
+       Post.fromJson(jsonDecode(response.body)); */
+      List<Post> result = (json.decode(response.body) as List)
+          .map((data) => Post.fromJson(data))
+          .toList();
+
+      setState(() {
+        widget.posts = result;
+      });
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
@@ -46,12 +56,10 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future<Post> futurePost;
-
-  @override
-  void initState() {
-    super.initState();
-    futurePost = fetchPost();
+  _HomePageState() {
+    // toda vez que a aplicação iniciar
+    // irá fazer a requisição na api
+    fetchPost();
   }
 
   @override
@@ -68,19 +76,27 @@ class _HomePageState extends State<HomePage> {
         ),
         title: Text('DreamPosts'),
       ),
-      body: Center(
-        child: FutureBuilder<Post>(
-          future: futurePost,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Text(snapshot.data.body);
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            return CircularProgressIndicator();
-          },
-        ),
-      ),
+      body: ListView.builder(
+          //constrói dinamicamente baseado no tamanho dos posts
+          itemCount: widget.posts.length,
+          itemBuilder: (BuildContext ctxt, int index) {
+            final post = widget.posts[index];
+            //constante utilizada para evitar a repetição de código
+
+            return Dismissible(
+              child: CheckboxListTile(
+                title: Text(post.title),
+                value: false, //valor true ou false
+                onChanged:
+                    (value) {}, //o que será executado quando clicar marcar a caixa
+              ),
+              key: Key(post.id.toString()),
+              background: Container(
+                color: Colors.red.withOpacity(0.2),
+              ), //o container quando criado toma o espaço total que é permitido
+              onDismissed: (direction) {},
+            );
+          }),
     );
   }
 }
@@ -205,3 +221,16 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
  */
+/* Center(
+        child: FutureBuilder<Post>(
+          future: futurePost,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data.body);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return CircularProgressIndicator();
+          },
+        ),
+      ), */
